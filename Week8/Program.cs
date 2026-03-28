7f5c7a2621970f3fdfc321ca5807e62fc7c4fc6b
oopiesii <polina.stupak.25@kse.org.ua>
2026-03-28 15:55:27 +0200

    program

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,9 +1,18 @@
-﻿
+﻿using System;
+namespace two;
 class Program
 {
     public static void Main()
     {
-        
+        FileResource file = new FileResource("report.txt");
+        NetworkResource network = new NetworkResource("api.company.local");
+        ResourceManager<Resource> manager = new ResourceManager<Resource>();
+        manager.Add(network);
+        manager.Add(file);
+        manager.OpenAll();
+        file.Dispose();
+        manager.CloseAll();
+        Console.WriteLine("Done.");
     }
     
 }
