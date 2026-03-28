c89524634b65dcbb8f0826d7a9cf8bd6d541292c
oopiesii <polina.stupak.25@kse.org.ua>
2026-03-28 16:16:18 +0200

    point

---
+++ Week8/point.cs
@@ -0,0 +1,11 @@
+using System;
+namespace three;
+public struct Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public void Print()
+    {
+        Console.WriteLine($"({X},{Y})");
+    }
+}
